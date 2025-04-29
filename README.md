f42de437cd2c38e5eaf25751758ee25b34ca13e7
Ondrej Kurák <ondrej.kurak.h@gmail.com>
2025-04-29 14:32:34 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# uses-version-check
-GitHub Actions Version Check
+# GitHub Actions Version Check
+
+This actions uses a simple bash script to check the versions of used actions in GitHub
+workflows.
+
+Example:
+
+```yaml
+name: Check Action Versions
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+  schedule:
+    - cron: "0 0 * * 0"
+jobs:
+  check_action_versions:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: H00N24/uses-version-check@main
+        with:
+          files: action.yml .github
+          ignore_version_regex: main
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ check_version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+headers='-H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"'
+# if GITHUB_TOKEN exists create auth header
+if [[ -n "$GITHUB_TOKEN" ]]; then
+    headers="$headers -H \"Authorization Bearer $GITHUB_TOKEN\""
+fi
+
+FILES="${FILES:-"$@"}"
+IGNORE_VERSION_REGEX="${IGNORE_VERSION_REGEX:-'main|master|latest'}"
+
+# if Use_error is set to true use error instead of warning
+if [[ "$USE_ERROR" == "true" ]]; then
+    MSG="error"
+else
+    MSG="warning"
+fi
+
+grep -hrPo '\s+uses:\s+\K(.*)' "$FILES" --include '*.yml' --include '*.yaml' | sort -u | while IFS= read -r action; do
+    action_name="$(echo "$action" | cut -d'@' -f1)"
+    action_version="$(echo "$action" | cut -d'@' -f2)"
+
+    # Check if action_version is empty or matches the ignore regex
+    if [[ -z "$action_version" || "$action_version" =~ $IGNORE_VERSION_REGEX ]]; then
+        echo "::debug::Ignoring action $action_name with version $action_version"
+        continue
+    fi
+
+    if [[ "$action_name" == *"/"* ]]; then
+        # Get the latest version from the GitHub API
+        latest_version="$(curl -Ls "$headers" "https://api.github.com/repos/$action_name/releases/latest" | jq -r '.tag_name')"
+
+        # Check if lastest version begins with action_version
+        if [[ "$latest_version." == "$action_version."* ]]; then
+            echo "::debug::Action $action_name is up to date. Current version: $action_version, Latest version: $latest_version"
+        else
+            echo "::$MSG title=$action_name is outdated::Current version: $action_version, Latest version: $latest_version"
+        fi
+    fi
+done
